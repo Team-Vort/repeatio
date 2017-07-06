b678bf1a2798f74ba571e1030aa57d88e13352a7
Himanshu Pant <himanshu.pn3@gmail.com>
2017-07-05 19:58:51 -0700

    added progress component

---
+++ public/components/progress.component.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  var module = angular.module('flash-card');
+
+  module.component('progress', {
+    templateUrl: '../templates/progress.component.html'
+    controller: 'progressCtrl'
+  })
+
+}());
