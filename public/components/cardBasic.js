4bafe7828d816c6c91c601c43a9ef24ee518baee
Semie Rogers <redrogers@gmail.com>
2017-07-06 08:03:41 -0700

    Add components and templates for basic, image, multiple choice, true/false and short answer cards

---
+++ public/components/cardBasic.js
@@ -0,0 +1,12 @@
+angular.module('flash-card')
+.controller('CardBasicCtrl', function() {
+
+})
+
+.component('cardBasic', {
+  controller: 'CardBasicCtrl',
+  bindings: {
+
+  },
+  templateUrl: './templates/cardBasic.html'
+});
